840cf15d053a17e2c0ba29ea5c6eba16ae5543ef
HojinLim <hojinim@gmail.com>
2024-02-24 23:01:26 +0900

    Design/이미지 크기

--- src/app/dogs/[slug]/page.tsx
+++ src/app/dogs/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const data = await getData(params.slug);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+    <main className="flex flex-col items-center justify-between p-12">
       <h1 className="text-3xl font-bold text-center mb-8">{params.slug}</h1>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
